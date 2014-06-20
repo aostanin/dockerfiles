9c0146f63aa92fa51f95be8c7e444362b129cfc7
Andrei Ostanin <aostanin@gmail.com>
2014-06-21 00:08:55 +0900

    remove pyc files on couchpotato start (fixes problems during startup)

--- services/couchpotato/start.sh
+++ services/couchpotato/start.sh
@@ -3,5 +3,6 @@
 set -e
 
 (cd /couchpotato && git pull)
+find /couchpotato -name "*.pyc" -exec rm -rf {} \;
 python2.7 /couchpotato/CouchPotato.py --daemon --console_log --data_dir=/data --config_file=/data/couchpotato.ini
 tail -f /data/logs/*.log
