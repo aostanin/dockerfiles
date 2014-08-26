51b70274b711a367e8307588cc66dbc4793a21f0
Andrei Ostanin <aostanin@gmail.com>
2014-08-26 22:59:41 +0900

    git pull couchpotato and headphones on start

--- services/couchpotato/start.sh
+++ services/couchpotato/start.sh
@@ -2,4 +2,6 @@
 
 set -e
 
+git -C /couchpotato pull
+
 python2.7 /couchpotato/CouchPotato.py --console_log --data_dir=/data --config_file=/data/couchpotato.ini
--- services/headphones/start.sh
+++ services/headphones/start.sh
@@ -2,4 +2,6 @@
 
 set -e
 
+git -C /headphones pull
+
 python2.7 /headphones/Headphones.py --datadir=/data --config=/data/headphones.ini
