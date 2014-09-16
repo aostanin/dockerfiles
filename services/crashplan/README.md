cc24c490119c57c85ff1efc63a457300bac0893b
Andrei Ostanin <aostanin@gmail.com>
2014-09-16 11:59:00 +0900

    configurable memory limit

--- services/crashplan/README.md
+++ services/crashplan/README.md
@@ -24,3 +24,9 @@ Backup service.
 
 UI service.
 
+## Environment Variables
+
+### `MAX_MEMORY_MB`
+
+The maximum memory limit in megabytes. Defaults to `1024`. Code42 [recommends](http://support.code42.com/CrashPlan/Latest/Troubleshooting/CrashPlan_Runs_Out_Of_Memory_And_Crashes) allocating 1 GB per 1 TB of storage.
+
--- services/crashplan/start.sh
+++ services/crashplan/start.sh
@@ -2,7 +2,9 @@
 
 set -e
 
+MAX_MEMORY_MB=${MAX_MEMORY_MB:-1024}
+
 [ ! -d /data/conf ] && cp -r /usr/local/crashplan/conf.default /data/conf
 
 cd /usr/local/crashplan
-nice -n 19 /usr/bin/java -Dfile.encoding=UTF-8 -Dapp=CrashPlanService -DappBaseName=CrashPlan -Xms20m -Xmx1024m -Djava.net.preferIPv4Stack=true -Dsun.net.inetaddr.ttl=300 -Dnetworkaddress.cache.ttl=300 -Dsun.net.inetaddr.negative.ttl=0 -Dnetworkaddress.cache.negative.ttl=0 -Dc42.native.md5.enabled=false -classpath /usr/local/crashplan/lib/com.backup42.desktop.jar:/usr/local/crashplan/lang com.backup42.service.CPService
+nice -n 19 /usr/bin/java -Dfile.encoding=UTF-8 -Dapp=CrashPlanService -DappBaseName=CrashPlan -Xms20m -Xmx${MAX_MEMORY_MB}m -Djava.net.preferIPv4Stack=true -Dsun.net.inetaddr.ttl=300 -Dnetworkaddress.cache.ttl=300 -Dsun.net.inetaddr.negative.ttl=0 -Dnetworkaddress.cache.negative.ttl=0 -Dc42.native.md5.enabled=false -classpath /usr/local/crashplan/lib/com.backup42.desktop.jar:/usr/local/crashplan/lang com.backup42.service.CPService
